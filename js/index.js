4dfec234db1c9f0c7153d752f4085173b13de9db
Ian <fairbrother.iang@yahoo.co.nz>
2017-10-24 12:24:06 +1300

    First commit

---
+++ js/index.js
@@ -0,0 +1,113 @@
+"use strict";
+
+(function() {
+  const TABLE_ROW_LENGTH = 100;
+  const TABLE_COLUMN_LENGTH = 100;
+
+  var data = {};
+
+  function storeValue(col, row, value) {
+    data[col + "_" + row] = value;
+  }
+
+  function retrieveValue(col, row) {
+    var value = data[col + "_" + row];
+
+    return value === undefined ? "" : value;
+  }
+
+  // function refreshValue(col, row) {
+  //   return data[col + "_" + row];
+  // }
+
+  // function refreshTable() {
+  //   for (var key in data) {
+  //     if (data.hasOwnProperty(key)) {
+  //       console.log(key.split("_") + " -> " + data[key]);
+  //     }
+  //   }
+  // }
+  let tableBody = document.getElementById("tableBody");
+
+  document.getElementById("refreshBtn").addEventListener("click", e => {
+    tableBody.innerHTML = "";
+    refreshTable();
+  });
+
+  function drawTable() {
+    //Table headings
+    for (let i = 1; i <= TABLE_COLUMN_LENGTH; i++) {
+      let cell = tableHeaderRow.insertCell(i);
+      cell.innerHTML = numberToLetters(i);
+    }
+
+    refreshTable();
+  }
+
+  function refreshTable() {
+    let tableHeaderRow = document.getElementById("tableHeaderRow");
+
+    for (let i = 1; i <= TABLE_ROW_LENGTH; i++) {
+      var row = tableBody.insertRow(i - 1);
+      row.setAttribute("data-row", i);
+
+      for (let j = 0; j <= TABLE_COLUMN_LENGTH; j++) {
+        var cell = row.insertCell(j);
+
+        //First cell in a row. Readonly displaying the row number.
+        if (j === 0) {
+          cell.innerHTML = i;
+        } else {
+          cell.innerHTML = retrieveValue(j, i);
+          cell.setAttribute("data-column", j);
+          //All other row editable
+          cell.setAttribute("contenteditable", "true");
+          //Might need to fix this for efficiency
+          cell.addEventListener("blur", e => {
+            var inputCell = e.target;
+
+            var col = inputCell.getAttribute("data-column");
+            var row = inputCell.parentElement.getAttribute("data-row");
+            storeValue(col, row, inputCell.innerHTML);
+          });
+        }
+      }
+    }
+  }
+
+  function getColumnNumber(str) {
+    var arr = str.split("").reverse();
+    //Fix 26 and 64
+    return arr
+      .map((x, i) => {
+        return (x.charCodeAt() - 64) * Math.pow(26, i);
+      })
+      .reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+      });
+  }
+
+  function numberToLetters(nNum) {
+    var result;
+    if (nNum <= 26) {
+      result = letter(nNum);
+    } else {
+      var modulo = nNum % 26;
+      var quotient = Math.floor(nNum / 26);
+      if (modulo === 0) {
+        result = letter(quotient - 1) + letter(26);
+      } else {
+        result = letter(quotient) + letter(modulo);
+      }
+    }
+
+    return result;
+  }
+
+  function letter(nNum) {
+    var a = "A".charCodeAt(0);
+    return String.fromCharCode(a + nNum - 1);
+  }
+
+  drawTable();
+})();
